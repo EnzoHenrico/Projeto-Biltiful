dba793360f787b0bfeee2621c909ad67c398a470
EnzoHenrico <enzo.mascia1@gmail.com>
2024-05-09 18:53:02 -0300

    Adicionado menu principal da aplicação

--- Projeto/Program.cs
+++ Projeto/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace Projeto
+﻿using Projeto.Cadastro;
+using Projeto.Cadastro.Manipuladores;
+
+namespace Projeto
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Biutiful Cosméticos\n");
             
+            Console.WriteLine("Qual módulo deseja acessar?");
+            Console.WriteLine("1 - Cadastro\n");
+            Console.WriteLine("2 - Vendas\n");
+            Console.WriteLine("3 - Compras\n");
+            Console.WriteLine("4 - Produção\n");
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    // TODO: MainCadastro.Acesso();
+                    break;
+                case "2":
+                    // TODO: MainVendas.Acesso();
+                    break;
+                case "3":
+                    // TODO: MainCompras.Acesso();
+                    break;
+                case "4":
+                    // TODO: MainProducao.Acesso();
+                    break;
+
+            }
         }
     }
 }
