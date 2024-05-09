0a080b49e7167b9885cfc91951826ac31b4237b7
EnzoHenrico <enzo.mascia1@gmail.com>
2024-05-09 19:01:19 -0300

    Inicio da estrutura basica de cadastro

--- Projeto/Cadastro/MainCadastro.cs
+++ Projeto/Cadastro/MainCadastro.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Projeto.Cadastro.Entidades;
+using Projeto.Cadastro.Manipuladores; 
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,53 @@ namespace Projeto.Cadastro
 {
     internal class MainCadastro
     {
+        private ManipuladorClientes ManipuladorClientes;
+        // ManipuladorProduto
+        // ManipularMPrima
+        // ManipularFornecedor
+
+        //public MainCadastro()
+        //{
+
+        //}
+
+        public static void Acesso()
+        {
+            Console.WriteLine("Selecione uma opção de cadastro: \n1- Clientes\n");
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("ok");
+                    break;
+            }
+        }
+
+        public void menu()
+        {
+
+        }
+        
+        public void menuCliente()  
+        {
+
+        }
+
+        public void menuMPrima()   
+        {
+
+        }
+
+        public void menuProduto()  
+        {
+
+        }
+
+        public void menuFornecedor()
+        
+        {
+
+        }
     }
 }
