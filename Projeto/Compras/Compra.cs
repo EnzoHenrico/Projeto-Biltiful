a9aa534385d8254dec86d91e66a9a9a11490be48
AnnyaB <anabordin2008@gmail.com>
2024-05-09 21:22:23 -0300

    instancia classes

---
+++ Projeto/Compras/Compra.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Compras
+{
+    internal class Compra
+    {
+        int Id { get; set; }
+        DateOnly DataCompra { get; set; }
+        string Fornecedor { get; set; }
+        int ValorTotal { get; set; }
+
+        public Compra()
+        {           
+        }
+
+        public Compra(int id, DateOnly dataCompra, string fornecedor, int valorTotal)
+        {
+            Id = id;
+            DataCompra = dataCompra;
+            Fornecedor = fornecedor;
+            ValorTotal = valorTotal;
+        }
+    }
+}
---
+++ Projeto/Compras/ItemCompra.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Compras
+{
+    internal class ItemCompra
+    {
+        int Id { get; set; }
+        DateOnly DataCompra { get; set; }
+        int MateriaPrima { get; set; }
+        int Quantidade { get; set; }
+        int ValorUnitario { get; set; }
+        int TotalItem { get; set; }
+
+        public ItemCompra()
+        {          
+        }
+
+        public ItemCompra(int id, DateOnly dataCompra, int materiaPrima, int quantidade, int valorUnitario, int totalItem)
+        {
+            Id = id;
+            DataCompra = dataCompra;
+            MateriaPrima = materiaPrima;
+            Quantidade = quantidade;
+            ValorUnitario = valorUnitario;
+            TotalItem = totalItem;
+        }
+    }
+}
