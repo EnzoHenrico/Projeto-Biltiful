bcecb07f7a8573c84b2527fa82d62ada7be483b3
EnzoHenrico <enzo.mascia1@gmail.com>
2024-05-11 13:11:17 -0300

    Adicionado classe para armezenar strings constantes da aplicacao

---
+++ Projeto/DicionarioStrings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto
+{
+    public static class DicionarioStrings
+    {
+        public static string Diretorio = @"C:\Biltiful\";
+        public static string ArquivoCliente = "Cliente.dat";
+    }
+}
