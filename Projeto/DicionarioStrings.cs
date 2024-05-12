2059ab2814be9b06e99019176d8856e788f729de
EnzoHenrico <enzo.mascia1@gmail.com>
2024-05-12 08:48:29 -0300

    Adicionado itens ao dicionario e manipulador de arquivos no escopo do projeto

--- Projeto/DicionarioStrings.cs
+++ Projeto/DicionarioStrings.cs
@@ -10,5 +10,10 @@ namespace Projeto
     {
         public static string Diretorio = @"C:\Biltiful\";
         public static string ArquivoCliente = "Cliente.dat";
+        public static string ArquivoFornecedor = "Fornecedor.dat";
+        public static string ArquivoProduto = "Cosmetico.dat";
+        public static string ArquivoMPrima = "Materia.dat";
+        public static string ArquivoInadinplentes = "Risco.dat";
+        public static string ArquivoBloqueados = "Bloqueado.dat";
     }
 }
